c9a628545d60007031ead5c72eced58ea212c1c9
Metin ÇİRİŞ <metin@metinciris.com.tr>
2024-08-22 23:57:18 +0300

    Update svs_to_png.py

--- svs_to_png.py
+++ svs_to_png.py
@@ -2,29 +2,26 @@ import openslide
 from PIL import Image
 import winsound
 
-def convert_svs_to_png(svs_file_path, output_png_path):
-    # SVS dosyasını aç
-    slide = openslide.OpenSlide(svs_file_path)
+# SVS dosya yollarını belirtin
+file1_path = r"C:\svs\dosya1.svs"
+file2_path = r"C:\svs\dosya2.svs"
 
-    # En büyük çözünürlükte resmi al
-    image = slide.read_region((0, 0), 0, slide.level_dimensions[0])
+# SVS dosyalarını açın
+slide1 = openslide.OpenSlide(file1_path)
+slide2 = openslide.OpenSlide(file2_path)
 
-    # Resmi PNG formatında kaydetme
-    image.save(output_png_path, 'PNG')
-    print(f"{output_png_path} kaydedildi.")
+# En büyük çözünürlükte resimleri al
+image1 = slide1.read_region((0, 0), 0, slide1.level_dimensions[0])
+image2 = slide2.read_region((0, 0), 0, slide2.level_dimensions[0])
 
-if __name__ == "__main__":
-    # SVS dosya yollarını belirtin
-    file1_path = r"C:\svs\dosya1.svs"
-    file2_path = r"C:\svs\dosya2.svs"
+# Resimleri PNG formatında kaydetme
+output_path1 = r"C:\svs\output_image1.png"
+output_path2 = r"C:\svs\output_image2.png"
+image1.save(output_path1, 'PNG')
+image2.save(output_path2, 'PNG')
 
-    # PNG olarak kaydedilecek yollar
-    output_path1 = r"C:\svs\output_image1.png"
-    output_path2 = r"C:\svs\output_image2.png"
+print(f"Resim 1 PNG olarak kaydedildi: {output_path1}")
+print(f"Resim 2 PNG olarak kaydedildi: {output_path2}")
 
-    # Dönüştürme işlemi
-    convert_svs_to_png(file1_path, output_path1)
-    convert_svs_to_png(file2_path, output_path2)
-
-    # Windows'ta işlem tamamlandığında bir ses çalın
-    winsound.Beep(1000, 1000)  # 1000 Hz frekansta, 1 saniye boyunca ses çalar
+# Windows'ta işlem tamamlandığında bir sistem sesi çalın
+winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
